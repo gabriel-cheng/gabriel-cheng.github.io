1ebed1dee223e21285ef3be602178c8da2669047
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2024-09-30 12:42:03 -0300

    chore: reworking header

--- assets/scripts/toggle.js
+++ assets/scripts/toggle.js
@@ -1,16 +1,37 @@
-const toggle_close = document.querySelector(".toggle_close");
-const options = document.querySelector(".header_list-options");
-const toggle = document.querySelector(".mobile_toggle-container");
+const header = document.querySelector(".header_main-container");
+const navigation = document.querySelector(".header-navigation");
+const navigation_subcontainer = document.querySelector(".header_navigation-subcontainer");
+let showSidebar = false;
 
+function toggleSidebar() {
+    showSidebar = !showSidebar;
 
-toggle_close.addEventListener("click", () => {
-    options.classList.remove("_active");
-    toggle_close.style.display = "none";
-    document.body.style = "overflow-y: scroll";
-});
+    if(showSidebar) {
+        navigation.style.marginLeft = "0";
+        navigation.style.animationName = "showSidebar";
 
-toggle.addEventListener("click", () => {
-    options.classList.add("_active");
-    toggle_close.style.display = "initial";
-    document.body.style = "overflow-y: hidden";
-});
+        document.body.style.overflow = "hidden";
+        
+        navigation_subcontainer.style.marginLeft = "0";
+        navigation_subcontainer.style.animationName = "showSidebar";
+        
+        return;
+    }
+    
+    document.body.style.overflow = "unset";
+    navigation.style.animationName = "";
+    navigation.style.marginLeft = "-100vw";
+    
+    navigation_subcontainer.style.animationName = "";
+    navigation_subcontainer.style.marginLeft = "-110vw";
+}
+
+function closeSidebar(event) {
+    const element = event.target;
+
+    if(element.classList.contains("header-navigation")) {
+        if(showSidebar) {
+            toggleSidebar();
+        }
+    }
+}
